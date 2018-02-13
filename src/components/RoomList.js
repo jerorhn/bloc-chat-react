809c702dfa3f61e44c5ee04770ba54a0873c1a63
Hannah Jeror <jerorhn@yahoo.com>
2018-02-13 16:51:45 -0500

    Chpt3- Add rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import './roomlist.css';
 
 class RoomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoomName: ''
     };
+    this.storeInput = this.storeInput.bind(this);
+    this.createRoom = this.createRoom.bind(this);
     this.roomsRef = this.props.firebase.database().ref('rooms');
   }
 
@@ -17,9 +21,32 @@ class RoomList extends Component {
     });
   }
 
+  createRoom(event) {
+    event.preventDefault();
+    this.roomsRef.push({
+      name: this.state.newRoomName
+    });
+    this.setState({newRoomName: ''});
+  }
+
+  storeInput(e) {
+    const newValue = e.target.value;
+    this.setState({newRoomName: newValue});
+  }
+
   render() {
     return(
-      this.state.rooms.map(num => num.name)
+      <div id='rooms'>
+      <h1>Bloc Chat</h1>
+        <form onSubmit={this.createRoom}>
+          <input type='text' onChange={this.storeInput}/>
+          <input type='submit'/>
+        </form>
+
+        <ul>
+          {this.state.rooms.map((num, index) => <li key={index}>{num.name}</li>)}
+        </ul>
+      </div>
     )
   }
 }
